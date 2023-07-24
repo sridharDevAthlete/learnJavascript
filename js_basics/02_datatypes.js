a0ab93fa94f5ec9978f94e0cdd340f8c5e4d8ca3
Sridhar Singaravelu <108050086+sridharDevAthlete@users.noreply.github.com>
2023-07-24 03:49:20 +0000

    datatypes

---
+++ js_basics/02_datatypes.js
@@ -0,0 +1,9 @@
+let name="Sridhar" // datatype String
+let age=23 //datatype number
+let isAthlete=true //boolean
+let careerGoal=null //kidding its a secret but its empty value
+let city;
+
+console.log(typeof isAthlete)
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
